921ffafd870a86be6e97f3ed3d2554821225cc2d
Alpha-87 <websiteiits@gmail.com>
2024-09-25 21:48:19 +0530

    2ND

--- application/config/config.php
+++ application/config/config.php
@@ -525,5 +525,5 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-$config['installed'] = TRUE;
+$config['installed'] = FALSE;
 $config['product_name'] = 'ramom_school_v6.7';
